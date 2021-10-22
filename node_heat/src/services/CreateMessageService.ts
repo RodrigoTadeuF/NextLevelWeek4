e3a8f83652541c11efa6806adc39bed180682432
RodrigoTadeuF <rodrigotfonsecas@outlook.com>
2021-10-22 17:19:54 -0300

    day 2 finished

--- node_heat/src/services/CreateMessageService.ts
+++ node_heat/src/services/CreateMessageService.ts
@@ -7,10 +7,11 @@ class CreateMessageService {
     const message = await prismaClient.message.create({
       data: {
         text,
-        user_id
-      }, include: {
-        user: true
-      }
+        user_id,
+      },
+      include: {
+        user: true,
+      },
     });
 
     const infoWS = {
@@ -19,14 +20,13 @@ class CreateMessageService {
       created_at: message.created_at,
       user: {
         name: message.user.name,
-        avatar_url: message.user.avatar_url
-      } 
-    }
+        avatar_url: message.user.avatar_url,
+      },
+    };
 
     io.emit("new_message", infoWS);
 
     return message;
   }
 }
-
 export { CreateMessageService }
